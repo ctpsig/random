9c190bdde7eac225783111273a5b1af66b52f66d
Charlie Siggelkow <csiggelkow@crescendocollective.com>
2019-02-14 13:08:38 -0600

    stuff

---
+++ my-hello-desc/hello.c
@@ -0,0 +1,17 @@
+/*
+ * hello.c - hello, world
+ *
+ * Placed in the public domain by Bryan O'Sullivan
+ *
+ * This program is not covered by patents in the United States or other
+ * countries.
+ */
+
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	printf("hello, world!\n");
+	printf("sure am glad I'm using Mercurial!\n");
+	return 0;
+}
---
+++ my-hello-not-cvs/hello.c
@@ -0,0 +1,17 @@
+/*
+ * hello.c
+ *
+ * Placed in the public domain by Bryan O'Sullivan
+ *
+ * This program is not covered by patents in the United States or other
+ * countries.
+ */
+
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	printf("hello, world!\n");
+	printf("sure am glad I'm not using CVS!\n");
+	return 0;
+}
